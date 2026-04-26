4b0a67e76dff7e1029f87468096950b9c074032e
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-04-26 09:22:49 +0200

    added Tests Cases for search and add endpoints in MovieService Class and added dependencies for them in pom.xml

--- src/test/Java/at/ac/fhcampuswien/MovieServiceTest.java
+++ src/test/Java/at/ac/fhcampuswien/MovieServiceTest.java
@@ -1,16 +1,27 @@
 package at.ac.fhcampuswien;
 
+import at.ac.fhcampuswien.controllers.MovieController;
 import at.ac.fhcampuswien.models.Movie;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import services.MovieService;
 
 import java.beans.BeanProperty;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class MovieServiceTest {
     private MovieService movieService;
@@ -92,4 +103,198 @@ public class MovieServiceTest {
         // list size should remain the same
         assertEquals(3, testMovies.size());
     }
-}
+
+        private HttpExchange mockExchangePost(String json) throws IOException {
+            HttpExchange exchange = mock(HttpExchange.class);
+
+            when(exchange.getRequestMethod()).thenReturn("POST");
+            when(exchange.getRequestURI()).thenReturn(java.net.URI.create("/api/movies/add"));
+            when(exchange.getRequestBody()).thenReturn(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)));
+
+            when(exchange.getResponseHeaders()).thenReturn(new Headers());
+            OutputStream responseStream = new ByteArrayOutputStream();
+            when(exchange.getResponseBody()).thenReturn(responseStream);
+
+            return exchange;
+        }
+
+        private HttpExchange mockExchangeGetSearch(String query, ByteArrayOutputStream response) throws IOException {
+            HttpExchange exchange = mock(HttpExchange.class);
+
+            when(exchange.getRequestMethod()).thenReturn("GET");
+            when(exchange.getRequestURI())
+                    .thenReturn(java.net.URI.create("/api/movies/search?" + query));
+
+            when(exchange.getResponseHeaders()).thenReturn(new Headers());
+            when(exchange.getResponseBody()).thenReturn(response);
+
+            return exchange;
+        }
+
+
+        @Test
+        void shouldAddMovieSuccessfully() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+                {
+                "title": "Inception",
+                "genre": "Sci-Fi",
+                "releaseYear": 2010
+                }
+                """;
+
+            HttpExchange exchange = mockExchangePost(json);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(201), anyLong());
+        }
+
+        @Test
+        void shouldReturnErrorIfMovieAlreadyExists() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+            {
+              "title": "Duplicate",
+              "genre": "Drama",
+              "releaseYear": 2010
+            }
+            """;
+
+            HttpExchange exchange1 = mockExchangePost(json);
+            HttpExchange exchange2 = mockExchangePost(json);
+
+            controller.handle(exchange1); // first add
+            controller.handle(exchange2); // duplicate
+
+            verify(exchange2).sendResponseHeaders(eq(400), anyLong());
+        }
+
+        @Test
+        void shouldReturnInvalidMovieDataIfYearTooLow() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+            {
+              "title": "Bad Movie",
+              "genre": "Horror",
+              "releaseYear": 1800
+            }
+            """;
+
+            HttpExchange exchange = mockExchangePost(json);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(400), anyLong());
+        }
+
+        @Test
+        void  shouldReturnInvalidMovieDataIfTitleMissing() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+            {
+              "genre": "Action",
+              "releaseYear": 2000
+            }
+            """;
+
+            HttpExchange exchange = mockExchangePost(json);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(400), anyLong());
+        }
+        @Test
+        void  shouldReturnInvalidMovieDataIfGenreMissing() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+            {
+              "Title": "Inception",
+              "releaseYear": 2000
+            }
+            """;
+
+            HttpExchange exchange = mockExchangePost(json);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(400), anyLong());
+        }
+        @Test
+        void  shouldReturnInvalidMovieDataIfReleaseYearMissing() throws IOException {
+            MovieController controller = new MovieController();
+
+            String json = """
+            {
+              "Title": "Inception",
+              "genre": "Action"
+            }
+            """;
+
+            HttpExchange exchange = mockExchangePost(json);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(400), anyLong());
+        }
+
+
+
+
+
+
+
+        @Test
+        void shouldFilterByTitle() throws IOException {
+            MovieController controller = new MovieController();
+
+            ByteArrayOutputStream response = new ByteArrayOutputStream();
+
+            HttpExchange exchange = mockExchangeGetSearch("title=dark", response);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(200), anyLong());
+
+            String result = response.toString().toLowerCase();
+            assertTrue(result.contains("dark"));
+        }
+        @Test
+        void shouldFilterByGenre() throws IOException {
+            MovieController controller = new MovieController();
+
+            ByteArrayOutputStream response = new ByteArrayOutputStream();
+
+            HttpExchange exchange = mockExchangeGetSearch("genre=action", response);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(200), anyLong());
+
+            String result = response.toString().toLowerCase();
+            assertTrue(result.contains("action"));
+        }
+
+        @Test
+        void shouldFilterByReleaseYear() throws IOException {
+            MovieController controller = new MovieController();
+
+            ByteArrayOutputStream response = new ByteArrayOutputStream();
+
+            HttpExchange exchange = mockExchangeGetSearch("releaseYear=2000", response);
+
+            controller.handle(exchange);
+
+            verify(exchange).sendResponseHeaders(eq(200), anyLong());
+
+            String result = response.toString().toLowerCase();
+            assertTrue(result.contains("2000"));
+        }
+    }
+
+
