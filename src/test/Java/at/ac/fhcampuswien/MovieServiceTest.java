67be658aa13927cfc1f0fc7c63a3c3c016dd85cf
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-04-27 12:31:40 +0200

    updated test cases to specifically test MovieService methods rather than mock Movie Controller http inputs

--- src/test/Java/at/ac/fhcampuswien/MovieServiceTest.java
+++ src/test/Java/at/ac/fhcampuswien/MovieServiceTest.java
@@ -173,6 +173,7 @@ public class MovieServiceTest {
         @Test
         void shouldFilterByReleaseYear() {
             String result = movieService.searchMovies(null, null, "1999");
+
             assertTrue(result.contains("1999"));
             assertFalse(result.contains("2007"));
         }
